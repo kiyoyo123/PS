b080ab853b2b855e3d33b0fa9de378bbd4618cda
Cheon Jun Woo <djaljsqamx@naver.com>
2021-07-21 06:11:16 +0900

    Add files via upload

---
+++ JW/B13305(주유소).cpp
@@ -0,0 +1,48 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int n;
+int cost[100000];
+int dist[100000];
+
+// 01. Input setting 
+void init(){
+	
+	cin >> n;
+	
+	for(int i=0; i<n-1; i++){
+		cin >> dist[i]; 
+	}
+	
+	for(int i=0; i<n; i++){
+		cin >> cost[i];
+	}	
+}
+
+int main(){
+	
+	init();
+	 
+	long long rst = 0;
+	
+	
+	int firstLoc = cost[0];
+	rst = (long long) (firstLoc * dist[0]);
+	 
+	for(int i=0; i<n-1; i++){
+		
+		if(firstLoc < cost[i]){
+			rst += (long long) firstLoc * dist[i+1];			
+		}else{
+			firstLoc = cost[i];
+			rst += (long long) firstLoc * dist[i+1];
+		}
+	}
+	cout << rst << endl;
+	
+	
+	 
+	
+	return 0;
+}
